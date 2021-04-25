97892fe4767ae50894447fe2c0ffea04f7ab7b9e
PascalPieper <79706631+PascalPieper@users.noreply.github.com>
2021-04-25 14:36:35 +0200

    inr

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,3 +1,5 @@
+
+/*
 #include "CompHeader.h"
 int main()
 {
@@ -13,18 +15,11 @@ int main()
 
     sf::RenderWindow window(sf::VideoMode(360, 200), "SFML works!");
 
-    //window.setSize(sf::Vector2u{ 1920, 1080 });
+    window.setSize(sf::Vector2u{ 1920, 1080 });
     window.setFramerateLimit(60);
     window.setVerticalSyncEnabled(true);
-    //sf::CircleShape shape(100.f);
-    //shape.setFillColor(sf::Color::Green);
 
     gm.CreateEntity<ScrollingBackground>(sf::Vector2f{ 0,0 });
-    /*
-    sf::Texture text;
-    text.loadFromFile("rainerSmall.jpg");
-    testa->EntitySprite.setTexture(text);
-    */
 
     while (window.isOpen())
     {
@@ -43,5 +38,85 @@ int main()
         gm.RefreshDeltatime();
     }
 
+    return 0;
+}*/
+
+#include <SFML/OpenGL.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include "CompHeader.h"
+int main()
+{
+    GameManager gm;
+    mat_m::SaveGameManager sgm("test");
+    AssetManager am;
+    gm.pSaveGameManager = &sgm;
+    gm.pAssetManager = &am;
+
+    sf::RenderWindow window(sf::VideoMode(360, 200), "SFML works!");
+    window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
+    ImGui::SFML::Init(window);
+
+    sf::Color bgColor;
+
+    float color[3] = { 0.f, 0.f, 0.f };
+
+    // let's use char array as buffer, see next part
+    // for instructions on using std::string with ImGui
+    char windowTitle[255] = "ImGui + SFML = <3";
+
+    window.setTitle(windowTitle);
+    window.resetGLStates(); // call it if you only draw ImGui. Otherwise not needed.
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Sample window"); // begin window
+
+                                       // Background color edit
+        if (ImGui::ColorEdit3("Background color", color)) {
+            // this code gets called if color value changes, so
+            // the background color is upgraded automatically!
+            bgColor.r = static_cast<sf::Uint8>(color[0] * 255.f);
+            bgColor.g = static_cast<sf::Uint8>(color[1] * 255.f);
+            bgColor.b = static_cast<sf::Uint8>(color[2] * 255.f);
+        }
+        float f;
+        if (ImGui::SliderFloat("float", &f, 0.0f, 1.0f))
+        {
+            std::cout << f;
+        }
+
+        // Window title text edit
+        ImGui::InputText("Window title", windowTitle, 255);
+
+        if (ImGui::Button("Update window title")) {
+            // this code gets if user clicks on the button
+            // yes, you could have written if(ImGui::InputText(...))
+            // but I do this to show how buttons work :)
+            window.setTitle(windowTitle);
+        }
+        ImGui::End(); // end window
+
+        window.clear(bgColor); // fill background with color
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
     return 0;
 }
